2685ca2ce740e10a43e228116824b0a503730b64
Marco Cardacci <marcoc@ticketech.local>
2016-03-10 17:16:26 -0500

    adds machine finding loop

--- send_mail.py
+++ send_mail.py
@@ -4,7 +4,7 @@ content="example email stuff"
 mail=smtplib.SMTP("smtp.gmail.com", 587)
 mail.ehlo()
 mail.starttls()
-mail.login('youremail@gmail.com', 'yourpassword')
-mail.sendmail("youremail@gmail.com","to-email@anything.com", content)
+mail.login('ticketechtest@gmail.com', 'locationswithpendingfiles')
+mail.sendmail("ticketechtest@gmail.com","ticketechtest@gmail.com", content)
 mail.close()
 
